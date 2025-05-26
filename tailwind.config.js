c8ecf817ecdd72126317319c8eb490a1aafa2712
daniperdomo <111296277+daniperdomo@users.noreply.github.com>
2025-05-26 13:20:59 -0400

    Base Emocional

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4361ee',
+        'primary-dark': '#3a56d4',
+      },
+    },
+  },
+  plugins: [],
+}
